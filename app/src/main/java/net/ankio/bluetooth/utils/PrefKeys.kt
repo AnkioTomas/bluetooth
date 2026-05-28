5bc3af53f02502cbd758be61e044fe07dc02a434
ankio <ankio@ankio.net>
2026-05-28 21:13:38 +0800

    :recycle: (core): 重构 PrefKeys.kt 配置项
    调整配置键常量定义，优化配置项命名和结构，提升代码可读性和维护性。

--- app/src/main/java/net/ankio/bluetooth/utils/PrefKeys.kt
+++ app/src/main/java/net/ankio/bluetooth/utils/PrefKeys.kt
@@ -5,19 +5,10 @@ object PrefKeys {
     const val PREF_MAC = "pref_mac"
     const val PREF_DATA = "pref_data"
     const val PREF_RSSI = "pref_rssi"
-    const val PREF_ENABLE = "pref_enable"
-    const val PREF_ENABLE_WEBDAV = "pref_enable_webdav"
-    const val PREF_AS_SENDER = "pref_as_sender"
     const val PREF_COMPANY = "pref_company"
-    const val PREF_MAC2 = "pref_mac2"
 
     const val WEBDAV_MODE = "webdav_mode"
     const val SIMULATE_MODE = "simulate_mode"
-    const val WEBDAV_SERVER = "webdav_server"
-    const val WEBDAV_USERNAME = "webdav_username"
-    const val WEBDAV_PASSWORD = "webdav_password"
     const val WEBDAV_LAST = "webdav_last"
-
-    const val HISTORY = "history"
-    const val APP_VERSION = "app_version"
+    const val SETTING_LANGUAGE = "setting_language"
 }
