1051f6dde4ef202628ca2ac10f5ad433e58a21f9
ankio <ankio@ankio.net>
2026-05-28 21:35:38 +0800

    :recycle: (core): 重构 HookUtils.kt 文件
    移除 BuildConfig 依赖，将工具类重构为对象结构，
    简化获取激活状态逻辑，删除无用的版本信息方法。

--- app/src/main/java/net/ankio/bluetooth/utils/HookUtils.kt
+++ app/src/main/java/net/ankio/bluetooth/utils/HookUtils.kt
@@ -1,16 +1,8 @@
 package net.ankio.bluetooth.utils
 
-import net.ankio.bluetooth.BuildConfig
-
 object HookUtils {
     //激活状态
     fun getActiveAndSupportFramework(): Boolean {
         return false
-    }
-    fun getAppVersion(): Int {
-        return BuildConfig.VERSION_CODE
-    }
-    fun getXposedVersion():Int{
-        return 82
     }
 }
