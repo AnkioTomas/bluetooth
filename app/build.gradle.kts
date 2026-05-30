ff582db49fce80adbfceea293424ee649bc85cb6
ankio <ankio@ankio.net>
2026-05-30 22:14:33 +0800

    :fire: (core): 移除BLE广播相关功能与配置
    删除了BLE广播器管理器和快速设置磁贴服务的实现，
    更新了混淆规则以适配Xposed模块，
    清理了相关的字符串资源和图标文件。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,6 +28,7 @@ android {
             isMinifyEnabled = true
             isShrinkResources = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
     compileOptions {
